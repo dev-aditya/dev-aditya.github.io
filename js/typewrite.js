65d9ebb582d57cd80996efd5e346f8ff73a58bed
देव @ WIS Workstation <adityadev@tuta.io>
2025-12-05 16:49:12 +0200

    Removed the dynamics typing and added the css fade in fade out

--- js/typewrite.js
+++ js/typewrite.js
@@ -1,58 +1,45 @@
-var TxtRotate = function(el, toRotate, period) {
+class TextRotator {
+  constructor(el, toRotate, period) {
     this.toRotate = toRotate;
     this.el = el;
     this.loopNum = 0;
-    this.period = parseInt(period, 5) || 1000;
-    this.txt = '';
-    this.tick();
-    this.isDeleting = false;
-  };
-  
-  TxtRotate.prototype.tick = function() {
-    var i = this.loopNum % this.toRotate.length;
-    var fullTxt = this.toRotate[i];
-  
-    if (this.isDeleting) {
-      this.txt = fullTxt.substring(0, this.txt.length - 1);
-    } else {
-      this.txt = fullTxt.substring(0, this.txt.length + 1);
-    }
-  
-    this.el.innerHTML = '<span class="wrap">'+this.txt+'</span>';
-  
-    var that = this;
-    var delta = 300 - Math.random() * 100;
-  
-    if (this.isDeleting) { delta /= 2; }
-  
-    if (!this.isDeleting && this.txt === fullTxt) {
-      delta = this.period;
-      this.isDeleting = true;
-    } else if (this.isDeleting && this.txt === '') {
-      this.isDeleting = false;
+    this.period = parseInt(period, 10) || 2000;
+    this.el.innerHTML = this.toRotate[0];
+    this.el.classList.add('visible');
+    this.cycle();
+  }
+
+  cycle() {
+    // Time to wait while text is fully visible
+    setTimeout(() => {
+      this.fadeOut();
+    }, this.period);
+  }
+
+  fadeOut() {
+    this.el.classList.remove('visible');
+    // Wait for the CSS transition (0.5s) to finish before changing text
+    setTimeout(() => {
       this.loopNum++;
-      delta = 500;
+      const i = this.loopNum % this.toRotate.length;
+      this.el.innerHTML = this.toRotate[i];
+      this.fadeIn();
+    }, 500); 
+  }
+
+  fadeIn() {
+    this.el.classList.add('visible');
+    this.cycle();
+  }
+}
+
+window.onload = function() {
+  const elements = document.getElementsByClassName('txt-rotate');
+  for (let i = 0; i < elements.length; i++) {
+    const toRotate = elements[i].getAttribute('data-rotate');
+    const period = elements[i].getAttribute('data-period');
+    if (toRotate) {
+      new TextRotator(elements[i], JSON.parse(toRotate), period);
     }
-  
-    setTimeout(function() {
-      that.tick();
-    }, delta);
-  };
-  
-  window.onload = function() {
-    var elements = document.getElementsByClassName('txt-rotate');
-    for (var i=0; i<elements.length; i++) {
-      var toRotate = elements[i].getAttribute('data-rotate');
-      var period = elements[i].getAttribute('data-period');
-      if (toRotate) {
-        new TxtRotate(elements[i], JSON.parse(toRotate), period);
-      }
-    }
-    // INJECT CSS
-    var css = document.createElement("style");
-    css.type = "text/css";
-    css.innerHTML = ".txt-rotate > .wrap { border-right: 0.08em solid #666 }";
-    document.body.appendChild(css);
-  };
-  
-  
+  }
+};
